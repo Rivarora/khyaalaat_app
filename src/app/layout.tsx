92d441c394307de037c01902e109626ba4d6846a
RIVA <riva0697.becse24@chitkara.edu.in>
2025-09-12 10:37:05 +0000

    dont add things which are causing error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,6 @@ import { ThemeProvider } from '@/components/providers/theme-provider';
 import { Toaster } from '@/components/ui/toaster';
 import { cn } from '@/lib/utils';
 import { Footer } from '@/components/footer';
-import { ParticleProvider } from '@/components/providers/particle-provider';
 
 export const metadata: Metadata = {
   title: 'Khyaalaat - A Poetry Portfolio',
@@ -33,11 +32,9 @@ export default function RootLayout({
           enableSystem={false}
           disableTransitionOnChange
         >
-          <ParticleProvider>
-            <div className="flex-grow">{children}</div>
-            <Toaster />
-            <Footer />
-          </ParticleProvider>
+          <div className="flex-grow">{children}</div>
+          <Toaster />
+          <Footer />
         </ThemeProvider>
       </body>
     </html>
