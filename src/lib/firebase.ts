ea72a0e887f28adf6aa50bdb49057c501672f408
RIVA <riva0697.becse24@chitkara.edu.in>
2025-09-26 15:51:13 +0000

    there is a project named khyaalaat on firebase i have made recently conn

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,13 +4,13 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  "projectId": "studio-4677355997-6e54c",
-  "appId": "1:507454652915:web:d22350a5794a1481e79286",
-  "storageBucket": "studio-4677355997-6e54c.appspot.com",
-  "apiKey": "AIzaSyB3K_5g5xcyPNKGOIq8XambSFbS8iACMnc",
-  "authDomain": "studio-4677355997-6e54c.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "507454652915"
+  "projectId": "khyaalaat-1929a",
+  "appId": "UPDATE_WITH_YOUR_APP_ID",
+  "storageBucket": "khyaalaat-1929a.appspot.com",
+  "apiKey": "UPDATE_WITH_YOUR_API_KEY",
+  "authDomain": "khyaalaat-1929a.firebaseapp.com",
+  "measurementId": "UPDATE_WITH_YOUR_MEASUREMENT_ID",
+  "messagingSenderId": "UPDATE_WITH_YOUR_MESSAGING_SENDER_ID"
 };
 
 // Initialize Firebase
